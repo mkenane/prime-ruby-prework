b9886f7e925e48c67e14ae55b8cd1ba7ab84569f
Mor Kenane <morkenan@hotmail.com>
2018-02-15 00:22:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,14 @@
 def prime?(number)
   
-  if number.abs == 1 || 2 
+  if number.abs == (1 || 2) 
     return false 
+  elsif number.abs == 3
+  return true 
   end 
   
   
  x =  Math.sqrt(number.abs).floor
- number_array = *(2..x)
+ number_array = *(4..x)
  
  number_array.each do |int|
  if number % int == 0
