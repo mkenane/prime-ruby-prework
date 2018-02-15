33c37bf5e386bf4e430ff0b7cf3e643546eeecdf
Mor Kenane <morkenan@hotmail.com>
2018-02-15 00:37:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(number)
+  
+ if (number.abs == 1 || number.abs == 0)
+    puts "#{number}"
+    return false 
+  elsif (number.abs == 2 || number.abs == 3)
+  return true 
+  end 
+  
+  
+  
+ x =  Math.sqrt(number.abs).floor
+ number_array = *(2..x)
+ 
+ number_array.each do |int|
+ if number % int == 0
+  return false
+  end 
+end 
+true 
+end 
