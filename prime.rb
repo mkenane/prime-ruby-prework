040466e0e17e9c1c967c757979714dd6652dccd8
Mor Kenane <morkenan@hotmail.com>
2018-02-15 00:03:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime(number)
+ x =  Math.sqrt(number).floor
+ number_array = *(2..x)
+ 
+ number_array.each do |int|
+ if number % int == 0
+  return false
+  end 
+end 
+true 
+end 
